02b801ae24644762bdcfb48e2a7aec6e84faf872
bbertka <bbertka@gmail.com>
2021-02-26 08:45:52 -0700

    updated server so it wont die all the time

--- bot.py
+++ bot.py
@@ -4,17 +4,29 @@ from routing import worker
 import datetime
 
 
+def streamRun():
+        try:
+                print("Slack Service: (re)initializing stream")
+		while(True)
+			print("Slack Service: pulse check healthy"):
+			time.sleep(60)
+
+        except Exception as e:
+                print ('Slack Service error: streamRun exception: %s' % e)
+                streamRun()
+
 def restartBot():
-        python = sys.executable
-        os.execl(python, python, * sys.argv)
+	python = sys.executable
+	os.execl(python, python, * sys.argv)
 
 
 if __name__=='__main__':
-        try:
-                worker.start()
+	try:
+		worker.start()
+		streamRun()
 
-        except Exception as e:
-                print('Slack Service error, main exception: %s' % e)
-                worker.stop()
-                restartBot()
+	except Exception as e:
+		print('Slack Service error, main exception: %s' % e)
+		worker.stop()
+		restartBot()
         worker.stop()
